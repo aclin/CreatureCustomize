d94bf719b759cc41f3e4e40efe2ec8a1c3bd42b3
aclin <devilsodo@gmail.com>
2011-05-19 19:41:02 -0700

    <merge> layout for monster page. Button opens settings.

--- src/com/alin/testcreature/TestCreature.java
+++ src/com/alin/testcreature/TestCreature.java
@@ -3,19 +3,27 @@ package com.alin.testcreature;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TestCreature extends Activity {
+public class TestCreature extends Activity implements View.OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        /*
-        Intent intent;
         
-        intent = new Intent().setClass(this, CostumeMenu.class);
-        startActivity(intent);
-        */
+        // Set an OnClickListener for the menu button on the main display
+        Button button = (Button) findViewById(R.id.config_button);
+        button.setOnClickListener(this);
+        
     }
+
+	@Override
+	public void onClick(View v) {
+		Intent intent;
+		intent = new Intent().setClass(this, CostumeMenu.class);
+		startActivity(intent);
+	}
     
 }
